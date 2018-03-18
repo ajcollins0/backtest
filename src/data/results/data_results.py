c25a85d4d232e21520a03f278d160d44cc926430
alex collins <alex.collins@tanium.com>
2018-03-18 18:03:55 -0400

    added inital values to be none, also added monte simulation to output 0-1.0 values rather than percentages

--- src/data/results/data_results.py
+++ src/data/results/data_results.py
@@ -5,10 +5,10 @@ from random import random
 class Trade:
 
     def __init__(self):
-        self.entry_date = ''
-        self.exit_date = ''
-        self.entry_price = 0.0
-        self.exit_price = 0.0
+        self.entry_date = None
+        self.exit_date = None
+        self.entry_price = None
+        self.exit_price = None
 
     def __init__(self, entry_date, entry_price, exit_date, exit_price):
         self.entry_date = entry_date
@@ -29,6 +29,8 @@ class Results:
     def print_results(self, full=False, graph=False, monte=True):
 
         capital = 10000
+        
+        monte_median = 0.0
 
         w_arr = np.array([])
         l_arr = np.array([])
@@ -58,30 +60,34 @@ class Results:
         print "Finishing Capital:", capital
 
         print
-        print "Mean Win", ave_win*100
-        print "Mean Loss", ave_loss*100
+        print "Mean Win", ave_win
+        print "Mean Loss", ave_loss
         print 
-        print "Median Win", np.median(w_arr)*100
-        print "Median Loss", np.median(l_arr)*100
+        print "Median Win", np.median(w_arr)
+        print "Median Loss", np.median(l_arr)
         print 
-        print "Max Win", np.amax(w_arr)*100
-        print "Max Loss", np.amax(l_arr)*100
+        print "Max Win", np.amax(w_arr)
+        print "Max Loss", np.amax(l_arr)
+        
         if full:
             print 
-            print "Min Win", np.amin(w_arr)*100
-            print "Min Loss", np.amin(l_arr)*100
+            print "Min Win", np.amin(w_arr)
+            print "Min Loss", np.amin(l_arr)
         print 
-        print "Standard Deviation of Wins", np.std(w_arr, ddof=1)*100
-        print "Standard Deviation of Loses", np.std(l_arr, ddof=1)*100
+        print "Standard Deviation of Wins", np.std(w_arr, ddof=1)
+        print "Standard Deviation of Loses", np.std(l_arr, ddof=1)
         print
         print "Number of wins:", w_count
         print "Number of loses:", l_count
 
         if monte:
-            self.__monte_test(num_trades, win_per, ave_win, ave_loss)
+            monte_median = self.__monte_test(num_trades, win_per, ave_win, ave_loss)
 
         if graph:
             gh.graph_results(self.trades)
+
+        print 
+        print capital, monte_median, num_trades, win_per, ave_win, ave_loss
 
     def __monte_test(self, num_trades, win_per, ave_win, ave_loss):
         
@@ -106,4 +112,5 @@ class Results:
         print "  max:", np.amax(monte_results)
         print "  min:", np.amin(monte_results)
 
+        return np.median(monte_results)
 
